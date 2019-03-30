fcafd60481bd65601146c50716b0788dd56ae907
Unknown <lollypop631@gmail.com>
2019-03-30 13:04:44 -0700

    convert png to jpg, more color code

--- colortest.py
+++ colortest.py
@@ -9,14 +9,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage.filters as sp
 
+for x in range (1,7):
+    img = plt.imread("./imgs/5Band" + str(x) + ".jpg")
+    
+    img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
+    green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
+    
+    plt.imshow(green_filter)
+    plt.show()
+    print("5Band" + str(x))
+
 for x in range (1,11):
     img = plt.imread("./imgs/r" + str(x) + ".jpg")
     
     img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
-    violet_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 140)
+    green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
     
-    plt.imshow(violet_filter)
+    plt.imshow(green_filter)
     plt.show()
+    print("r" + str(x))
+    
 
 #green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
 #red_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 100)
@@ -25,3 +37,4 @@ for x in range (1,11):
 #orange_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] > 70) * (img_blur[:,:,1] < 150)* (img_blur[:,:,2] < 100)
 #white_filter = (img_blur[:,:,0] > 200) * (img_blur[:,:,1] > 200) * (img_blur[:,:,2] > 200)
 #blue_filter = (img_blur[:,:,0] < 80) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 100)
+#violet_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 140)

bb81073a2d31cbf22602482b304f0b3285f3080a
Unknown <lollypop631@gmail.com>
2019-03-30 15:40:34 -0700

    integrated crop function, and for loops

--- colortest.py
+++ colortest.py
@@ -5,43 +5,138 @@ Created on Sat Mar 30 00:35:26 2019
 @author: lolly
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.ndimage.filters as sp
+CONST_BLACK = 0 
+CONST_BROWN = 1 
+CONST_RED = 2 
+CONST_ORANGE = 3 
+CONST_YELLOW = 4 
+CONST_GREEN = 5 
+CONST_BLUE = 6 
+CONST_VIOLET = 7 
+CONST_GRAY = 8 
+CONST_WHITE = 9 
+CONST_SILVER = 10 
+CONST_GOLD = 11 
+ 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import matplotlib.colors as colors 
+import scipy.ndimage.filters as sp 
+import scipy.signal as sig 
+import scipy.spatial.distance as similarity 
 
-for x in range (1,10):
-    img = plt.imread("./imgs/5Band" + str(x) + ".jpg")
+def crop (name): 
+    img = plt.imread(name) 
+     
+    plt.imshow(img) 
+    plt.show() 
+ 
+    batchWidth = 8 
+ 
+    (h, w) = img.shape[:2] 
+    w2 = int(np.ceil(w / float(batchWidth))) 
+ 
+    mods = w % batchWidth 
+    if mods == 0: 
+        pw = (0, 0) 
+    else: 
+        pw = (0, batchWidth - mods) 
+ 
+    img = np.stack((np.pad(img[:,:,0], ((0,h), pw), 'symmetric'),np.pad(img[:,:,1], ((0,h), pw), 'symmetric'),np.pad(img[:,:,2], ((0,h), pw), 'symmetric')), axis=-1) 
+ 
+ 
+    averaged_vals = np.zeros([1,w2,3]) 
+    for i in range(w2): 
+        r = np.mean(img[:, i * batchWidth:(i + 1) * batchWidth, 0]) 
+        b = np.mean(img[:, i * batchWidth:(i + 1) * batchWidth, 1]) 
+        g = np.mean(img[:, i * batchWidth:(i + 1) * batchWidth, 2]) 
+ 
+        averaged_vals[0][i][0] = r 
+        averaged_vals[0][i][1] = b 
+        averaged_vals[0][i][2] = g 
+        
+    averaged_vals = np.stack((averaged_vals[0],averaged_vals[0],averaged_vals[0],averaged_vals[0],averaged_vals[0],averaged_vals[0]), axis=0) 
+    plt.imshow(averaged_vals.astype(int)) 
+    plt.show() 
     
-    img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
-    brown_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 50)
-    
-    plt.imshow(brown_filter)
-    plt.show()
-    print("5Band" + str(x))
+    return averaged_vals 
 
-for x in range (1,11):
-    img = plt.imread("./imgs/r" + str(x) + ".jpg")
-    
-    img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
-    brown_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 50)
-    
-    plt.imshow(brown_filter)
-    plt.show()
-    print("r" + str(x))
-    
-img = plt.imread("./imgs/test.jpg")
-ing_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
-gray_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,0] < 200) * (img_blur[:,:,1] > 50) * (img_blur[:,:,1] < 200) * (img_blur[:,:,2] > 50) * (img_blur[:,:,2] < 200)
-
-plt.imshow(gray_filter)
-plt.show()
-print("test")
-#green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
-#red_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 100)
-#black_filter = (img_blur[:,:,0] < 65) * (img_blur[:,:,1] < 65) * (img_blur[:,:,2] < 65)
-#yellow_filter = (img_blur[:,:,0] > 130) * (img_blur[:,:,1] > 130) * (img_blur[:,:,2] < 80)
-#orange_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] > 70) * (img_blur[:,:,1] < 150)* (img_blur[:,:,2] < 100)
-#white_filter = (img_blur[:,:,0] > 200) * (img_blur[:,:,1] > 200) * (img_blur[:,:,2] > 200)
-#blue_filter = (img_blur[:,:,0] < 80) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 100)
-#violet_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 140)
-#brown_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 50)
+def showColor (array, color): 
+      
+     def black(): 
+         black_filter = (array[:,:,0] < 65) * (array[:,:,1] < 65) * (array[:,:,2] < 65) 
+         plt.imshow(black_filter) 
+         plt.show() 
+         print(color) 
+     def brown(): 
+         brown_filter = (array[:,:,0] > 50) * (array[:,:,1] < 100) * (array[:,:,2] < 50) 
+         plt.imshow(brown_filter)  
+         plt.show() 
+         print(color) 
+     def red(): 
+         red_filter = (array[:,:,0] > 170) * (array[:,:,1] < 100) * (array[:,:,2] < 100) 
+         plt.imshow(red_filter) 
+         plt.show() 
+         print(color) 
+     def orange(): 
+         orange_filter = (array[:,:,0] > 170) * (array[:,:,1] > 70) * (array[:,:,1] < 150)* (array[:,:,2] < 100) 
+         plt.imshow(orange_filter) 
+         plt.show() 
+         print(color) 
+     def yellow(): 
+         yellow_filter = (array[:,:,0] > 130) * (array[:,:,1] > 130) * (array[:,:,2] < 80) 
+         plt.imshow(yellow_filter) 
+         plt.show() 
+         print(color) 
+     def green(): 
+         green_filter = (array[:,:,0] < 120) * (array[:,:,1] > 90) * (array[:,:,2] < 120) 
+         plt.imshow(green_filter) 
+         plt.show() 
+         print(color) 
+     def blue(): 
+         blue_filter = (array[:,:,0] < 80) * (array[:,:,1] < 150) * (array[:,:,2] > 100) 
+         plt.imshow(blue_filter) 
+         plt.show() 
+         print(color) 
+     def violet(): 
+         violet_filter = (array[:,:,0] > 50) * (array[:,:,1] < 150) * (array[:,:,2] > 140) 
+         plt.imshow(violet_filter) 
+         plt.show() 
+         print(color) 
+     def gray(): 
+         gray_filter = (array[:,:,0] > 50) * (array[:,:,0] < 200) * (array[:,:,1] > 50) * (array[:,:,1] < 200) * (array[:,:,2] > 50) * (array[:,:,2] < 200) 
+         plt.imshow(gray_filter) 
+         plt.show() 
+         print(color) 
+     def white(): 
+         white_filter = (array[:,:,0] > 200) * (array[:,:,1] > 200) * (array[:,:,2] > 200) 
+         plt.imshow(white_filter) 
+         plt.show() 
+         print(color) 
+         
+     filters = {  0: black, 
+                 1: brown, 
+                 2: red, 
+                 3: orange, 
+                 4: yellow, 
+                 5: green, 
+                 6: blue, 
+                 7: violet, 
+                 8: gray, 
+                 9: white, 
+                 #10: silver, 
+                 #11: gold, 
+             } 
+ 
+     filters[color] ()  
+   
+# Four-band resistors  
+          
+for x in range (1,11): 
+    pic = "./imgs/r" + str(x) + ".jpg" 
+    print("r" + str(x)) 
+    array = crop(pic) 
+     
+    for y in range (0,10): 
+        showColor(array,y) 
+         

bc2e722f8c82363c8a3fcfb46deb0e85eebd469b
Unknown <melodyc18113765@tas.tw>
2019-03-30 15:08:29 -0700

    updated color identifier

--- src/identifycolor.py
+++ src/identifycolor.py
@@ -5,27 +5,41 @@ Created on Sat Mar 30 12:17:42 2019
 
 @author: melodychen
 """
+
 class colorIdentifier:
     from clarifai.rest import ClarifaiApp
     from clarifai.rest import Image as ClImage
-    
     def __init__(self,path):
-        app = ClarifaiApp(api_key='')
-        model = app.models.get('color')
-        response = model.predict_by_filename(path)
-        
+        self.path = path
+        self.app = ClarifaiApp(api_key='3fbee996b23d4094b14d0ffe2b8f96e2')
+        self.model = self.app.models.get('color')
+        self.response = self.model.predict_by_filename(self.path)
+        self.colorlist = []
     def convertToRGB(self, hex):
         h = hex.lstrip('#')
         return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
-    def getRGB(self):
-        #convert to our own dictionary
-        colors = response['outputs'][0]['data']['colors']
+    def setList(self):
+        colors = self.response['outputs'][0]['data']['colors']
         length = len(colors)
         for x in range(length):
-            rgb_tup = convertToRGB(colors[x]['raw_hex'])
+            rgb_tup = self.convertToRGB(colors[x]['raw_hex'])
             processed_tup=(rgb_tup,colors[x]['value'])
-            colorlist.append(processed_tup)
-        return colorlist
+            self.colorlist.append(processed_tup)
+    def getRGB(self):
+        self.setList()
+        #convert to our own dictionary
+        return self.colorlist
+    def getDominant(self):
+        self.setList()
+        maxV = 0;
+        maxC = self.colorlist[0][0]
+        for x in range(len(self.colorlist)):
+            current = self.colorlist[x][1]
+            if(current>maxV):
+               maxV=current
+               maxC=self.colorlist[x][0]
+        return maxC
+            
 
 
 

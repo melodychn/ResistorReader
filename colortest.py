fc858e8976ea80cdba00dc04114e99e4780b8903
Unknown <lollypop631@gmail.com>
2019-03-30 13:46:22 -0700

    more colors

--- colortest.py
+++ colortest.py
@@ -9,13 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage.filters as sp
 
-for x in range (1,7):
+for x in range (1,10):
     img = plt.imread("./imgs/5Band" + str(x) + ".jpg")
     
     img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
-    green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
+    brown_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 50)
     
-    plt.imshow(green_filter)
+    plt.imshow(brown_filter)
     plt.show()
     print("5Band" + str(x))
 
@@ -23,13 +23,19 @@ for x in range (1,11):
     img = plt.imread("./imgs/r" + str(x) + ".jpg")
     
     img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
-    green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
+    brown_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 50)
     
-    plt.imshow(green_filter)
+    plt.imshow(brown_filter)
     plt.show()
     print("r" + str(x))
     
+img = plt.imread("./imgs/test.jpg")
+ing_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
+gray_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,0] < 200) * (img_blur[:,:,1] > 50) * (img_blur[:,:,1] < 200) * (img_blur[:,:,2] > 50) * (img_blur[:,:,2] < 200)
 
+plt.imshow(gray_filter)
+plt.show()
+print("test")
 #green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
 #red_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 100)
 #black_filter = (img_blur[:,:,0] < 65) * (img_blur[:,:,1] < 65) * (img_blur[:,:,2] < 65)
@@ -38,3 +44,4 @@ for x in range (1,11):
 #white_filter = (img_blur[:,:,0] > 200) * (img_blur[:,:,1] > 200) * (img_blur[:,:,2] > 200)
 #blue_filter = (img_blur[:,:,0] < 80) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 100)
 #violet_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 140)
+#brown_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 50)

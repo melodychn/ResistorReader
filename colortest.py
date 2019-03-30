e13089ec860c25d4f610195a4c9f1753f9135199
Unknown <lollypop631@gmail.com>
2019-03-30 12:01:25 -0700

    found more color values

--- colortest.py
+++ colortest.py
@@ -7,15 +7,21 @@ Created on Sat Mar 30 00:35:26 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.ndimage.filters as sp
 
-for x in range (1,12):
-    #img1 = plt.imread("./imgs/r1.jpg")
+for x in range (1,11):
     img = plt.imread("./imgs/r" + str(x) + ".jpg")
-    red_filter = (img[:,:,0] > 150 ) * (img[:,:,1] < 100) * (img[:,:,2] < 100)
-    plt.imshow(red_filter)
+    
+    img_blur = np.stack((sp.gaussian_filter(img[:,:,0], 5),sp.gaussian_filter(img[:,:,1], 5),sp.gaussian_filter(img[:,:,2], 5)), axis=-1)
+    violet_filter = (img_blur[:,:,0] > 50) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 140)
+    
+    plt.imshow(violet_filter)
     plt.show()
 
-#green_filter = (img[:,:,1] > 0) * (img[:,:,0] < 100) * (img[:,:,2] < 100)
-#plt.imshow(green_filter)
-#plt.show()
-
+#green_filter = (img_blur[:,:,0] < 120) * (img_blur[:,:,1] > 90) * (img_blur[:,:,2] < 120)
+#red_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] < 100) * (img_blur[:,:,2] < 100)
+#black_filter = (img_blur[:,:,0] < 65) * (img_blur[:,:,1] < 65) * (img_blur[:,:,2] < 65)
+#yellow_filter = (img_blur[:,:,0] > 130) * (img_blur[:,:,1] > 130) * (img_blur[:,:,2] < 80)
+#orange_filter = (img_blur[:,:,0] > 170) * (img_blur[:,:,1] > 70) * (img_blur[:,:,1] < 150)* (img_blur[:,:,2] < 100)
+#white_filter = (img_blur[:,:,0] > 200) * (img_blur[:,:,1] > 200) * (img_blur[:,:,2] > 200)
+#blue_filter = (img_blur[:,:,0] < 80) * (img_blur[:,:,1] < 150) * (img_blur[:,:,2] > 100)
